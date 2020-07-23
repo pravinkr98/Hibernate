f999467def2453916bc149a18034188a83956146
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-07-23 13:36:14 +0530

    1st-Application-R&D

--- HBProj1-SavingObject/src/com/ps/test/PersistObjectTest.java
+++ HBProj1-SavingObject/src/com/ps/test/PersistObjectTest.java
@@ -8,7 +8,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.ps.entity.Product;
 
-public class SavingObjectTest {
+public class PersistObjectTest {
 
 	public static void main(String[] args) {
 		Configuration cfg=null;
@@ -22,32 +22,28 @@ public class SavingObjectTest {
 		//create configuration object
 		cfg=new Configuration();
 		System.out.println(cfg.getProperties());
-		System.out.println("...................................");
 		//map hibernate configuration file with cfg object
 		cfg.configure("com/ps/cfgs/Product.cfg.xml");
-//		cfg.configure();
 		System.out.println(cfg.getProperties());
 		//create session factory object
 		factory=cfg.buildSessionFactory();
-		System.out.println("Factory class object name :: "+factory.getClass());
+		System.out.println("SessionFactory class object name :: "+factory.getClass());
 		//open session object
 		ses=factory.openSession();
 		System.out.println("Session class object name :: "+ses.getClass());
 		//add product details in Product object
 		prod=new Product();
-		prod.setPid(353);
-		prod.setPname("Table");
-		prod.setPrice(7500);
-		prod.setQty(4);
-		//start transcation
+		prod.setPid(355);
+		prod.setPname("Aalmirah");
+		prod.setPrice(15500);
+		prod.setQty(2);
+		//start transaction
 		try {
 			tx=ses.beginTransaction();
 			System.out.println("Transaction class object name :: "+tx.getClass());
 			//save/insert object data(Product details) into database table
-//			ses.save(prod);
-			idVal=(int) ses.save(prod);
-			System.out.println("Generated id value :: "+idVal);
-			System.out.println("1");
+			ses.persist(prod);
+			System.out.println("Id value :: "+prod.getPid());
 			flag=true;
 		}
 		catch(HibernateException he) {
@@ -57,9 +53,7 @@ public class SavingObjectTest {
 		finally {
 			//commit or rollback transaction
 			if(flag==true) {
-				System.out.println("2");
 				tx.commit();
-				System.out.println("3");
 				System.out.println("Object is saved successfully.");
 			}
 			else {
