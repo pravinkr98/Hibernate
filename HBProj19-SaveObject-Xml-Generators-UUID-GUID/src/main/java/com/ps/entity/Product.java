57a121521da530c46d135bc60b7df87f9a4f1a8d
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-08-29 16:35:12 +0530

    Generators-UUID-GUID

---
+++ HBProj19-SaveObject-Xml-Generators-UUID-GUID/src/main/java/com/ps/entity/Product.java
@@ -0,0 +1,17 @@
+package com.ps.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Product {
+
+	private String pid;
+	private String pname;
+	private Float price;
+	private Float qty;
+		
+}
