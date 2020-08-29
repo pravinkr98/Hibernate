9030569cdfd5b6c4d19d7a74efff1d8182e3d519
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-08-29 16:34:33 +0530

    Generators-CentralizedDB

---
+++ HBProj18-SaveObject-Xml-Generators-CentralizedDB/src/main/java/com/ps/test/GeneratorIdIdentityTest2.java
@@ -0,0 +1,60 @@
+package com.ps.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.ps.entity.Product;
+import com.ps.utility.HibernateUtil;
+
+public class GeneratorIdIdentityTest2 {
+
+	public static void main(String[] args) {
+		Session ses=null;
+		Transaction tx=null;
+		Product prod=null;
+		boolean flag=false;
+		int idVal=0;
+		
+		//get session object from HibernateUtil factory class
+		ses=HibernateUtil.getSession();
+		//add product details in Product object
+		prod=new Product();
+		//prod.setPid(402);
+		prod.setPname("Dressing");
+		prod.setPrice(8000.0f);
+		prod.setQty(6.0f);
+		
+		//start transcation
+		try {
+			tx=ses.beginTransaction();
+			//save object
+			idVal=(int) ses.save(prod);
+			flag=true;
+			System.out.println("Generated id value :: "+idVal);
+			System.out.println("id value :: "+prod.getPid());
+		}
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			//commit or rollback Tx
+			if(flag) {
+				tx.commit();
+				System.out.println("Object is saved successfully.");
+			}
+			else {
+				tx.rollback();
+				System.out.println("Object saving is failed.");
+			}
+			//close session object
+			HibernateUtil.closeSession(ses);
+			//close session factory object
+			HibernateUtil.closeSessionFactory();
+		}
+	}//main
+
+}//class
